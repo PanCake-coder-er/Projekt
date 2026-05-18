db50494edcb88436e052c9bcdbb7c00ed2e04ad1
PanCake-coder-er <praktytant107@gmail.com>
2026-05-18 17:49:25 +0200

    Update kuma_stats.sh

--- kuma_stats.sh
+++ kuma_stats.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+KUMA_URL="http://192.168.8.191:3001/api/push/3UocDo6BLD?status=up&msg=OK&ping="
+
+while true; do
 CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 RAM_USAGE=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
-KUMA_URL="http://192.168.8.191:3001/api/push/3UocDo6BLD?status=up&msg=OK&ping="
 curl -s "${http://192.168.8.191:3001/dashboard}?status=up&msg=CPU:${CPU_USAGE}%,RAM:${RAM_USAGE}%"
+sleep 60
+done
